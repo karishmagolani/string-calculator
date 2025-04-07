35f4d0b881d06a2c44ee493408363ae4da7ff17d
Karishma Golani <karishma@shipmnts.com>
2025-04-07 23:01:41 +0530

    feat: handle newline characters as valid delimiters

--- src/utils/stringCalculator.ts
+++ src/utils/stringCalculator.ts
@@ -1,5 +1,5 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0;
-  const numArr = numbers.split(",");
+  const numArr = numbers.split(/[\n,]/);
   return numArr.reduce((sum, curr) => sum + parseInt(curr), 0);
 }
