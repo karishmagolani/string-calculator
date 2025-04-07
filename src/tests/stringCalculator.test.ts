6bddee4a4016257f9c876cdfabcc08c6d32a6660
Karishma Golani <karishma@shipmnts.com>
2025-04-07 22:54:33 +0530

    test: return sum of multiple comma-separated numbers

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -4,11 +4,16 @@ describe("String Calculator", () => {
   test("returns 0 for empty string", () => {
     expect(add("")).toBe(0);
   });
+
   test("returns number when a single number is provided", () => {
     expect(add("1")).toBe(1);
   });
 
   test("returns sum when two numbers are provided", () => {
     expect(add("1,5")).toBe(6);
+  });
+
+  test("returns sum of multiple comma-separated numbers", () => {
+    expect(add("1,2,3,4")).toBe(10);
   });
 });
