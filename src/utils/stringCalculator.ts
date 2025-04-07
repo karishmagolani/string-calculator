5d5a6201c4320a4e7ba722a424fa5ef4f39371b8
Karishma Golani <karishma@shipmnts.com>
2025-04-07 22:52:59 +0530

    feat: support addition of two comma-separated numbers

--- src/utils/stringCalculator.ts
+++ src/utils/stringCalculator.ts
@@ -1,5 +1,5 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0;
-  const num = parseInt(numbers, 10);
-  return isNaN(num) ? 0 : num;
+  const numArr = numbers.split(",");
+  return numArr.reduce((sum, curr) => sum + parseInt(curr), 0);
 }
