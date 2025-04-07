ca7de80b7484d63825d8ba3db3f13da3103548b6
Karishma Golani <karishma@shipmnts.com>
2025-04-08 00:28:08 +0530

    feat: parse and use custom delimiter

--- src/utils/stringCalculator.ts
+++ src/utils/stringCalculator.ts
@@ -1,5 +1,14 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0;
-  const numArr = numbers.split(/[\n,]/);
+  let delimiterRegex = /[\n,]/;
+  let numString = numbers;
+
+  if (numbers.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    const customDelimiter = numbers.substring(2, delimiterEndIndex);
+    delimiterRegex = new RegExp(`[${customDelimiter}\n]`);
+    numString = numbers.substring(delimiterEndIndex + 1);
+  }
+  const numArr = numString.split(delimiterRegex);
   return numArr.reduce((sum, curr) => sum + parseInt(curr), 0);
 }
