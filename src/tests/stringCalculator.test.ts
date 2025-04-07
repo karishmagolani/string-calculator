d2aca66cdee48ae127729948b9323723a4c09dd6
Karishma Golani <karishma@shipmnts.com>
2025-04-07 22:49:24 +0530

    refactor: test case

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -4,12 +4,11 @@ describe("String Calculator", () => {
   test("returns 0 for empty string", () => {
     expect(add("")).toBe(0);
   });
-});
+  test("returns number when a single number is provided", () => {
+    expect(add("1")).toBe(1);
+  });
 
-test("returns number when a single number is provided", () => {
-  expect(add("1")).toBe(1);
-});
-
-test("returns sum when two numbers are provided", () => {
-  expect(add("1,5")).toBe(6);
+  test("returns sum when two numbers are provided", () => {
+    expect(add("1,5")).toBe(6);
+  });
 });
